7e11b225e41a36692fa63db69058c2541c113d02
Roberto Giungato <roberto@giungato.it>
2014-02-05 18:52:43 +0100

    added webcam page and modified script to manage SX and DX buttons

--- python/script.py
+++ python/script.py
@@ -161,8 +161,16 @@ def sendLum(arg0):
 
 @webiopi.macro
 def Servo1(arg0):
-	os.system("echo \"23=0.1\" > /dev/pi-blaster")
-	sleep(2)
+        os.system("echo \"23=0.1\" > /dev/pi-blaster")
+        # sleep(2)
+        # os.system("echo \"23=0.2\" > /dev/pi-blaster")
+        os.system("echo \"23=0\" > /dev/pi-blaster")
+        return ("%s" % ("done"))
+
+@webiopi.macro
+def Servo2(arg0):
+	# os.system("echo \"23=0.1\" > /dev/pi-blaster")
+	# sleep(2)
 	os.system("echo \"23=0.2\" > /dev/pi-blaster")
 	os.system("echo \"23=0\" > /dev/pi-blaster")
 	return ("%s" % ("done"))
